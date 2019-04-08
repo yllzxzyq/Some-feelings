7834694a19b299994b2f1bfa5fa998eef84974ce
yllzxzyq <49398283+yllzxzyq@users.noreply.github.com>
2019-04-08 20:03:36 +0800

    Add files via upload

---
+++ old book list.py
@@ -0,0 +1,45 @@
+import requests
+import re
+
+def getHTML(url):
+    try:
+        kv={'user-agent':'Mozilla/5.0'}
+        r=requests.get(url,headers=kv,timeout=30)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def parsePage(ilt, html):
+    try:
+        plt= re.findall(r'price="(\d.*)"',html)
+        tlt= re.findall(r'alt="<b>(.*)</b>(.*)" error="0"',html)
+        for i in range(len(plt)):
+            ilt.append([plt[i],tlt[i][0]+tlt[i][1]])
+    except:
+        return ""
+
+
+def printGoodsList(ilt):
+    tplt = "{:4}\t{:8}\t{:16}"
+    print(tplt.format("序号","价格","商品名称"))
+    count = 0
+    for g in ilt:
+        count = count+1 
+        print(tplt.format(count,g[0],g[1]))
+        
+def main():
+    depth = 3
+    start_url= 'http://search.kongfz.com/product_result/?key=%E6%B5%8B%E7%BB%98&status=0&_stpmt=eyJzZWFyY2hfdHlwZSI6ImFjdGl2ZSJ9&pagenum='
+    infoList=[]
+    for i in range(1,depth):
+        try:
+            url= start_url+str(i)
+            html = getHTML(url)
+            parsePage(infoList,html)
+        except:
+            continue
+    printGoodsList(infoList)
+
+main()
